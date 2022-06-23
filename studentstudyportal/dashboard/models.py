1350133ff36ffd8d81094141efcb672360480f4d
munga114 <mungai.mbugua@student.moringaschool.com>
2022-06-23 11:56:36 +0300

    update views.py file

--- studentstudyportal/dashboard/models.py
+++ studentstudyportal/dashboard/models.py
@@ -7,6 +7,9 @@ class Notes(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.title
+    
     class Meta:
         verbose_name = "notes"
         verbose_name_plural = "notes"
