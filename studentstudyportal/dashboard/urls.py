eaa8ad11ab8ed68fc1b1e736919aa8b00c40ad95
munga114 <mungai.mbugua@student.moringaschool.com>
2022-06-23 10:27:08 +0300

    update urls.py file

--- studentstudyportal/dashboard/urls.py
+++ studentstudyportal/dashboard/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('notes',views.notes)
+    path('',views.home,name="home"),
+    path('notes',views.notes,name="notes")
 ]
