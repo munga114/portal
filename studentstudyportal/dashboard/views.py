00e0597abee7862bbd1a676c1c6b878b79ffc320
munga114 <mungai.mbugua@student.moringaschool.com>
2022-06-23 13:13:43 +0300

    update notes function

--- studentstudyportal/dashboard/views.py
+++ studentstudyportal/dashboard/views.py
@@ -1,3 +1,4 @@
+from pydoc import describe
 from django.shortcuts import render
 from . forms import *
 
@@ -6,6 +7,13 @@ def home(request):
     return render(request,'dashboard/home.html')
 
 def notes(request):
+    if request.method == "POST":
+        form = NotesForm(request.Post)
+        if form.is_valid():
+            notes = Notes(user=request.user,title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request,f"Notes added from {request.user.username} successfully")
+
     form = NotesForm()
     notes = Notes.objects.filter(user=request.user)
     context = {'notes':notes,'form':form}
